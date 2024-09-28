25674b8eea68f419d3265d1eb49fd2186587da68
adam <adam@adam>
2024-09-28 20:57:32 -0300

    update: Validando administrador padrão

---
+++ Dominio/Servicos/AdministradorServico.cs
@@ -0,0 +1,21 @@
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
+namespace dio_minimal_api.Dominio.Servicos
+{
+    public class AdministradorServico : IAdministradorServico
+    {
+        private readonly DbContexto _contexto;
+        public AdministradorServico(DbContexto contexto)
+        {
+            _contexto = contexto;
+        }
+        public Administrador? Login(LoginDTO loginDTO)
+        {
+            return _contexto.Administradores.Where(x => 
+            x.Email == loginDTO.Email && x.Senha == loginDTO.Senha).FirstOrDefault();
+        }
+    }
+}
