15ce3ed1da59dd26de0fcfebda49e5e487ba2a1d
adam <adam@adam>
2024-09-28 22:27:09 -0300

    feat: Configurando Swagger na Aplicação

--- Interfaces/IVeiculoServico.cs
+++ Interfaces/IVeiculoServico.cs
@@ -4,13 +4,13 @@ using MinimalApi.DTOs;
 namespace MinimalApi.Dominio.Interfaces;
 
 public interface IVeiculoServico {
-    List<Veiculo> Todos (int pagina, string? nome = null, string? marca = null);
+    List<Veiculo> Todos (int pagina = 1, string? nome = null, string? marca = null);
     
-    Veiculo BuscaPorId(int id);
+    Veiculo? BuscaPorId(int id);
 
-    Veiculo Incluir(Veiculo veiculo);
+    void Incluir(Veiculo veiculo);
 
-    Veiculo Atualizar(Veiculo veiculo);
+    void Atualizar(Veiculo veiculo);
 
     void Apagar(Veiculo veiculo);
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DbContexto>(options => {
     options.UseMySql(
         builder.Configuration.GetConnectionString("mysql"),
@@ -26,6 +29,9 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
     else
         return Results.Unauthorized();
 });
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
